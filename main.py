5f189fc0995b128da4b9610da830c32e2f8c75b0
Tobias Nusser <tobiasnusser@web.de>
2019-02-26 16:06:07 +0100

    Added hurdle generation

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from tkinter import *
 import time
+import random
 import tkinter.messagebox
+
 
 def exit():
     root.destroy()
 
+
 def create_figure(game):
     canvas.create_rectangle(20, 20, 100, 120)
+
 
 def start_game(event):
     root.destroy()
@@ -17,12 +21,9 @@ def start_game(event):
     game.bind("<space>", create_figure(game))
 
 
-
-
-
-
 def move_figure(event):
-    canvas.move(1,5,0)
+    canvas.move(1, 5, 0)
+
 
 root = Tk()
 root.geometry("800x600")
@@ -37,11 +38,10 @@ root.bind("<Right>", move_figure)
 # start_btn.bind("<Button-1>", start_game)
 #
 # end_btn = Button(root, text="Quit", fg="Red", command=exit)
-
-canvas = Canvas(root, width=800, height=600, bd=0, highlightthickness=0)
+width = 900
+height = 600
+canvas = Canvas(root, width=width, height=height, bd=0, highlightthickness=0)
 canvas.pack()
-
-
 
 # start_btn.pack()
 # end_btn.pack()
@@ -52,19 +52,21 @@ class Player:
     def __init__(self, canvas, hurdle):
         self.canvas = canvas
         self.hurdle = hurdle
+        self.hurdle_list = []
         self.id = canvas.create_oval(10, 10, 25, 25, fill="Red")
         self.canvas.move(self.id, 150, 274)
         self.x = 0
-        self.y = -1
+        self.y = -2
         self.canvas.bind_all("<space>", self.jump)
         self.jumping = False
+        self.hit = False
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
         pos = self.canvas.coords(self.id)
         if self.hit_hurdle(pos):
-            print("Hit")
-
+            self.hit = True
+            self.canvas.create_text(200, 200, text="Game over")
 
     def jump(self, event):
         self.jumping = True
@@ -73,50 +75,59 @@ class Player:
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] < 200:
-            self.y = 1
+            self.y = 2
         if pos[3] >= 299:
             self.jumping = False
-            self.y = -1
+            self.y = -2
 
     def hit_hurdle(self, player_pos):
         hurdle_pos = self.canvas.coords(self.hurdle.id)
         if player_pos[2] >= hurdle_pos[0] and player_pos[3] >= hurdle_pos[1] and player_pos[0] <= hurdle_pos[2]:
-            time.sleep(0.5)
             return True
         else:
             return False
 
-class Lane:
-    def __init__(self, canvas):
-        self.canvas = canvas
-        self.id = canvas.create_line(0, 300, 800, 300)
 
 class Hurdle:
     def __init__(self, canvas):
         self.canvas = canvas
         self.id = canvas.create_rectangle(10, 10, 25, 25, fill="Black")
         self.canvas.move(self.id, 450, 274)
-        self.x = -1
+        self.x = -2
         self.y = 0
 
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[0] <= 0:
-            self.x = 1
+            self.x = 2
         if pos[2] > 500:
-            self.x = -1
+            self.x = -2
 
+
+class Background:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.id = canvas.create_line(0, height / 2, width, height / 2)
+
+
+lane = Background(canvas)
 hurdle = Hurdle(canvas)
 player = Player(canvas, hurdle)
-lane = Lane(canvas)
 
 
 while 1:
-    player.draw()
-    hurdle.draw()
-    if player.jumping:
-        player.draw_jump()
+    if not player.hit:
+        player.draw()
+        player.hurdle.draw()
+        if player.jumping:
+            player.draw_jump()
+    curr_hurdle_pos = canvas.coords(player.hurdle.id)
+    if curr_hurdle_pos[2] < canvas.coords(player.id)[0] - 10:
+        canvas.delete(player.hurdle.id)
+        player.hurdle = Hurdle(canvas)
+        player.hurdle.draw()
+
     root.update_idletasks()
     root.update()
     time.sleep(0.01)
